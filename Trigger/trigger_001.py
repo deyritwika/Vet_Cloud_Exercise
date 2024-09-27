7804d6a6c04cca88c1d9ae7bac2e2c513cc1cc84
deyritwika <43910382+deyritwika@users.noreply.github.com>
2024-09-27 17:08:44 -0400

    Create trigger_001.py

---
+++ Trigger/trigger_001.py
@@ -0,0 +1,27 @@
+!pip install requests==2.31.0
+!pip install pydicom==2.3.0
+
+import pydicom
+import requests
+
+image_path = "https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/0ed704ae-2c3a-480f-b651-03890e7f4884.dcm"
+ds = pydicom.dcmread("https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/0ed704ae-2c3a-480f-b651-03890e7f4884.dcm")
+
+url = "https://zz59wxzzz1.execute-api.us-east-2.amazonaws.com/default/sample_analytics"
+api_key = "q7aokbLl8n5jl7O0eOiGg3ddxdivkzKy48gpQZiX"
+
+def trigger_analytics(sample_id, request_type):
+  headers = {
+      "api_key": api_key,
+      "Content-Type": "application/json" 
+  }
+  payload = {
+      "sample_id": sample_id,
+      "request_type":request_type 
+  }
+
+  response = requests.post(url, headers=headers, json=payload)
+  if response.status_code == 200:
+      print("Analytics data sent successfully!")
+  else:
+      print(f"Error sending analytics data: {response.status_code}, {response.text}")
