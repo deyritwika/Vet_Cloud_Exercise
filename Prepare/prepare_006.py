b39ce32435330650502538520aae972d339b465e
deyritwika <43910382+deyritwika@users.noreply.github.com>
2024-09-27 16:53:39 -0400

    Create prepare_006.py

---
+++ Prepare/prepare_006.py
@@ -0,0 +1,27 @@
+!pip install pydicom==2.3.0
+
+import pydicom
+import datetime
+
+def modify_dicom_image(image_path, metadata, sample_id, attempt_num):
+
+    ds= pydicom.dcmread("https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/6d4855a4-67af-4dab-b439-f8ac5ff931ff.dcm")
+
+    ds.PatientName = metadata.get("PatientName")
+    ds.RequestSpecies = metadata.get("RequestSpecies")
+    ds.TestType = metadata.get("TestType")
+    ds.ClinicName = metadata.get("ClinicName")
+    ds.StudyDate = datetime.now().strftime('%Y%m%d')
+
+    ds.save_as("e644c339-6b50-435e-a1f5-2cc2e01459b0.dcm")
+    print("DICOM image modified successfully")
+
+    return "e644c339-6b50-435e-a1f5-2cc2e01459b0.dcm"
+
+    sample_id= ' ref0000011tes-cbc-002'
+    attempt_num= '006'
+
+    image_path= "https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/6d4855a4-67af-4dab-b439-f8ac5ff931ff.dcm"
+
+    modified_image_path= modify_dicom_image(image_path, metadata, sample_id, attempt_num)
+    print(modified_image_path)
