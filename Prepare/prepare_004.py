879bbed0d774922e16fc5c55532cdd820b882941
deyritwika <43910382+deyritwika@users.noreply.github.com>
2024-09-27 16:59:35 -0400

    Update prepare_004.py

--- Prepare/prepare_004.py
+++ Prepare/prepare_004.py
@@ -18,7 +18,7 @@ def modify_dicom_image(image_path, metadata, sample_id, attempt_num):
 
     return "e644c339-6b50-435e-a1f5-2cc2e01459b0.dcm"
 
-    sample_id= ' ref0000011tes-cbc-002'
+    sample_id= ' ref0000011tes-cbc-004'
     attempt_num= '004'
 
     image_path= "https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/5c6e2d1a-2ad4-450a-ae70-e01a0442677a.dcm"
