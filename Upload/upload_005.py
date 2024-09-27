ba2eec66633c56564336ec17f1a82ac628bead51
deyritwika <43910382+deyritwika@users.noreply.github.com>
2024-09-27 17:05:14 -0400

    Create upload_005.py

---
+++ Upload/upload_005.py
@@ -0,0 +1,23 @@
+# step 3: Upload Images to API Endpoint
+!pip install requests==2.31.0
+!pip install pydicom==2.3.0
+
+import pydicom
+import requests
+
+image_path = "https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/661c8904-26b7-48e8-b65c-8028d14069fd.dcm"
+ds = pydicom.dcmread("https://github.com/deyritwika/Vet_Cloud_Exercise/blob/main/DICOM%20IMAGES/661c8904-26b7-48e8-b65c-8028d14069fd.dcm")
+ 
+url = 'https://d2gveozzsod0j7.cloudfront.net/instances/'
+
+headers = {
+    "authorization": "Basic YWRtaW46WVpjdz1icHlGMChQN2FURQ==",
+}
+
+with open(image_path, 'rb') as file:
+    response = requests.post(url, headers=headers, data=file, timeout=3600)
+
+if response.status_code == 200:
+    print('successful!')
+else:
+    print('failed:', response.status_code, response.text)
